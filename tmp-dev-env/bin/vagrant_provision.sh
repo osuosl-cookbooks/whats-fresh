9f29fc9f5524936e0daaf598805e1e408efa43bb
Ian Kronquist <iankronquist@osuosl.org>
2015-01-05 16:37:44 -0800

    Add additional project configuration
    * Install ius repos, python 2.7
    * Create database
    * Start postgres
    * Install postgis extensions

--- tmp-dev-env/bin/vagrant_provision.sh
+++ tmp-dev-env/bin/vagrant_provision.sh
@@ -4,8 +4,20 @@
 
 cd /home/vagrant/project/
 
+sudo yum -y install curl
+
+curl -O http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/ius-release-1.0-13.ius.centos6.noarch.rpm
+
+sudo yum install -y ius-release-1.0-13.ius.centos6.noarch.rpm 
+
 #install the yum-ius repo which has python 2.7
-sudo yum -y install python-virtualenv git postgresql python27
+sudo yum -y install python-virtualenv git postgresql-server postgresql python27
+
+sudo service postgresql initdb << end
+    whats_fresh
+end
+sudo service postgresql start
+
 runuser -l postgres -c 'psql whats_fresh -c "CREATE EXTENSION postgis;"'
 
 
